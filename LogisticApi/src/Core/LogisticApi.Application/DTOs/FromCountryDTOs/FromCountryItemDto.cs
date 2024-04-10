a8a45d90d215a2102d3d51964c208e6c0ca48b21
Alikeremov <tu83bnh9m@code.edu.az>
2024-04-10 19:56:07 +0400

    FromCountryCrud was create

---
+++ LogisticApi/src/Core/LogisticApi.Application/DTOs/FromCountryDTOs/FromCountryItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticApi.Application.DTOs
+{
+    public class FromCountryItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
