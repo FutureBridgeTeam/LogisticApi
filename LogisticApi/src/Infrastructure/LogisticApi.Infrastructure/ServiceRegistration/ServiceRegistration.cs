528270e26839649430301c1531c3a36482aa35d8
HuseynMikayilzade <145683431+HuseynMikayilzade@users.noreply.github.com>
2024-06-09 13:15:12 +0400

    Change Swagger gen

--- LogisticApi/src/Infrastructure/LogisticApi.Infrastructure/ServiceRegistration/ServiceRegistration.cs
+++ LogisticApi/src/Infrastructure/LogisticApi.Infrastructure/ServiceRegistration/ServiceRegistration.cs
@@ -1,6 +1,9 @@
 ﻿using LogisticApi.Application.Abstraction.Services;
 using LogisticApi.Infrastructure.Implementations.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +14,34 @@ namespace LogisticApi.Infrastructure.ServiceRegistration
 {
     public static class ServiceRegistration
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
         {
             services.AddScoped<ICloudinaryService,CloudinaryService>(); 
             services.AddScoped<IEmailService,EmailService>();
             services.AddScoped<IJwtTokenService,JwtTokenService>();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+
+                    ValidAudience = configuration["Jwt:Audience"],
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecurityKey"])),
+                    LifetimeValidator = (_, expired, key, _) => key != null ? expired > DateTime.UtcNow : false
+
+                };
+            });
+            services.AddAuthorization();
 
             return services;    
         }
--- LogisticApi/src/Presentation/LogisticApi.API/Program.cs
+++ LogisticApi/src/Presentation/LogisticApi.API/Program.cs
@@ -1,6 +1,7 @@
 using LogisticApi.Application.ServiceRegistration;
 using LogisticApi.Infrastructure.ServiceRegistration;
 using LogisticApi.Persistance.ServiceRegistration;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +10,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddPersistanceServices(builder.Configuration);
 builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
